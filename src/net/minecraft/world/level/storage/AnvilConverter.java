94b2db8c48067b6df9fe08b3b1f11d719002101b
jazzwhistle <jdcraftmcpe@gmail.com>
2017-03-26 01:08:58 +0100

    Clean up

--- src/net/minecraft/world/level/storage/AnvilConverter.java
+++ src/net/minecraft/world/level/storage/AnvilConverter.java
@@ -12,7 +12,7 @@ import java.io.File;
 public class AnvilConverter {
 
     public static void main(String[] args) {
-    //String[] args = {"/Users/jazzwhistle/PocketMine/worlds","bcspawn"};
+
         File baseFolder;
         try {
             baseFolder = new File(args[0]);
